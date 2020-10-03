02bfbf6f79a511fd3be884d82213c0fefca7c2bd
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-10-03 10:39:32 -0300

    Métodos estáticos

--- conta.py
+++ conta.py
@@ -1,9 +1,9 @@
 class Conta():
     def __init__(self, numero, titular, saldo, limite):
-		self.__numero = numero
-		self.__titular = titular
-		self.__saldo = saldo
-		self.__limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
         print(f"Saldo R${self.__saldo} do titular {self.__nome}")
@@ -11,12 +11,24 @@ class Conta():
     def depositar(self, valor):
         self.__saldo += valor
 
+    def podeSacar(self, valorDeSaque):
+        valorDisponivelParaSaque = self.__saldo + self.__limite
+        return valorDeSaque <= valorDisponivelParaSaque
+
     def sacar(self, valor):
-        self.__saldo -= valor
+        if(self.__podeSacar(valor)):
+            self.__saldo -= valor
+            print("Saque efetuado com sucesso!")
+        else:
+            print(f"O valor R${valor} passou do limite.")
 
     def transferir(self, valor, destino):
-        self.sacar(valor)
-        destino.depositar(valor)
+        if (self.__podeSacar(valor)):
+            self.sacar(valor)
+            destino.depositar(valor)
+            print("Transferência efetuada com sucesso!")
+        else:
+            print(f"O valor R${valor} passou do limite.")
 
     @property
     def saldo(self):
@@ -33,3 +45,11 @@ class Conta():
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigoBanco():
+        return "001"
+
+    @staticmethod
+    def codigosBancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
